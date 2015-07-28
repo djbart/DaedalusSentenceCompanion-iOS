82a933143cc6c47a9538b78f47a1988ceaf2db9c
Bart Waeterschoot <bart.waeterschoot@gmail.com>
2015-07-28 12:57:21 +0200

    Added progress view & dice randomizers

--- Daedalus Sentence Companion/ViewControllerRoundStart.h
+++ Daedalus Sentence Companion/ViewControllerRoundStart.h
@@ -10,8 +10,20 @@
 
 @interface ViewControllerRoundStart : UIViewController
 
+//Game settings
 @property (weak, nonatomic) IBOutlet UILabel *gameTimerLabel;
+@property (weak, nonatomic) IBOutlet UIProgressView *gameTimerProgress;
 @property double gameTimerValue;
 @property int gameTimerRemainingInSeconds;
+@property int gameTimerInitialInSeconds;
+
+//Round settings
+@property (weak, nonatomic) IBOutlet UILabel *roundTimerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *disableLocationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cardsToDrawLabel;
+
+- (IBAction)RerollRoundTime:(id)sender;
+- (IBAction)RerollDisabledLocation:(id)sender;
+- (IBAction)RerollCardsToDraw:(id)sender;
 
 @end
--- Daedalus Sentence Companion/ViewControllerRoundStart.m
+++ Daedalus Sentence Companion/ViewControllerRoundStart.m
@@ -13,7 +13,10 @@
 - (void) viewDidLoad
 {
     [super viewDidLoad];
+    _gameTimerInitialInSeconds = _gameTimerValue * 60;
     _gameTimerRemainingInSeconds = _gameTimerValue * 60;
+    
+    [self updateTime];
     
     //Set up a timer that calls the updateTime method every second to update the label
     NSTimer *timer;
@@ -22,6 +25,11 @@
                                            selector:@selector(updateTime)
                                            userInfo:nil
                                             repeats:YES];
+    
+    //Roll dice
+    _roundTimerLabel.text = [self RollDie];
+    _disableLocationLabel.text = [self RollDie];
+    _cardsToDrawLabel.text = [self RollDie];
 }
 
 -(void)updateTime
@@ -30,9 +38,29 @@
     NSInteger minutes = _gameTimerRemainingInSeconds / 60;
     
     //Update the label with the remaining time
-    _gameTimerLabel.text = [NSString stringWithFormat:@"%02i min %02i sec", minutes, seconds];
+    _gameTimerLabel.text = [NSString stringWithFormat:@"%02li min %02li sec", (long)minutes, (long)seconds];
+    
+    //Update the progress view
+    _gameTimerProgress.progress = (float)_gameTimerRemainingInSeconds / (float)_gameTimerInitialInSeconds;
     
     _gameTimerRemainingInSeconds--;
+}
+
+- (IBAction)RerollRoundTime:(id)sender {
+    _roundTimerLabel.text = [self RollDie];
+}
+
+- (IBAction)RerollDisabledLocation:(id)sender {
+    _disableLocationLabel.text = [self RollDie];
+}
+
+- (IBAction)RerollCardsToDraw:(id)sender {
+    _cardsToDrawLabel.text = [self RollDie];
+}
+
+- (NSString *)RollDie
+{
+    return[NSString stringWithFormat:@"%i", (arc4random() % 6) + 1];
 }
 
 @end
