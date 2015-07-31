664db6bdad7569b5a94a0f38b08fc296c1852d51
Bart Waeterschoot <bart.waeterschoot@gmail.com>
2015-07-31 13:44:21 +0200

    Added round view controller

---
+++ Daedalus Sentence Companion/ViewControllerRound.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerRound.h
+//  Daedalus Sentence Companion
+//
+//  Created by Bart Waeterschoot on 31/07/15.
+//  Copyright (c) 2015 Cripplefish Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerRound : UIViewController
+
+@end
